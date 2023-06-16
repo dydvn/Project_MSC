9dc9e3a3094efdaf936f1ff98c6a9a9b9bf1e8a2
김용원 <dydvn@naver.com>
2023-06-17 02:17:18 +0900

    Initial commit

---
+++ Project_MSC/app/src/main/java/com/lemonsheep/project_msc/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.lemonsheep.project_msc.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
