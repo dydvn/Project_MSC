67b89b143cfcfcf317384ad0e453215eea09955e
김용원 <dydvn@naver.com>
2023-06-22 00:33:06 +0900

    연습중

--- Project_MSC/app/src/main/java/com/lemonsheep/project_msc/MainActivity.kt
+++ Project_MSC/app/src/main/java/com/lemonsheep/project_msc/MainActivity.kt
@@ -2,10 +2,19 @@ package com.lemonsheep.project_msc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv: TextView = findViewById(R.id.txt_test) as TextView
+        val btn_todolist: Button =findViewById(R.id.btn_to_do_list) as Button
+
+        btn_todolist.setOnClickListener{
+            tv.text  = "안녕"
+        }
     }
 }
