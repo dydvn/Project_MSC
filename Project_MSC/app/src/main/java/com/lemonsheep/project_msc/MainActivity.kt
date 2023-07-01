5a603ab93df5f5bef12f6d4066cdb1f9b45fc1db
김용원 <dydvn@naver.com>
2023-07-01 23:18:00 +0900

    연습중~

--- Project_MSC/app/src/main/java/com/lemonsheep/project_msc/MainActivity.kt
+++ Project_MSC/app/src/main/java/com/lemonsheep/project_msc/MainActivity.kt
@@ -4,17 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.util.Timer
+import kotlin.concurrent.timer
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.txt_test) as TextView
-        val btn_todolist: Button =findViewById(R.id.btn_to_do_list) as Button
+        var sec: Int = 0
+        var IsRun: Boolean = false
+        var timerTask: Timer? = null
 
-        btn_todolist.setOnClickListener{
-            tv.text  = "안녕"
+        val tv: TextView = findViewById(R.id.txt_test) as TextView
+        val btn_todolist: Button = findViewById(R.id.btn_to_do_list) as Button
+
+        btn_todolist.setOnClickListener {
+
+            IsRun = !IsRun
+
+            if (IsRun) {
+                timerTask = kotlin.concurrent.timer(period = 1000) {
+                    sec++
+                    runOnUiThread {
+                        tv.text = sec.toString()
+                    }
+                }
+            } else {
+                timerTask?.cancel()
+            }
         }
     }
 }
