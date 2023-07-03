6246620c3c071d8b9f61aff0b632c09b45da8245
김용원 <dydvn@naver.com>
2023-07-03 14:26:18 +0900

    연습하면서 진행중

--- Project_MSC/app/src/main/java/com/lemonsheep/project_msc/MainActivity.kt
+++ Project_MSC/app/src/main/java/com/lemonsheep/project_msc/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.lemonsheep.project_msc
 
-import android.R
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TableLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.io.Console
+import java.util.Random
+import java.util.Timer
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +20,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tableLayout:TableLayout = findViewById(R.id.tablelayout_name1) as TableLayout
+        // 선언부
+
+        val board: TableLayout = findViewById(R.id.tablelayout_name1) as TableLayout
+        val txtYear: TextView = findViewById(R.id.txt_year) as TextView
+        val btnYearBefore: Button = findViewById(R.id.btn_year_before) as Button
+        val btnYearAefore: Button = findViewById(R.id.btn_year_after) as Button
+
+        var year:Int = 2023
+
+        fun SetYearText(value:Int){
+            txtYear.text = value.toString()
+        }
+        fun IsLeapYear(value:Int):Boolean{
+            if(value%400 == 0)
+                return true
+            else if(value% 100 == 0)
+                return false
+            else if(value%4 == 0)
+                return true
+
+            return false
+        }
+
+        // for test
+        var LOG_TAG:String = "My Log : "
+
+        // ~선언부
 
 
 
+        // 구현부
 
+        SetYearText(year) // 기기에 저장돼있는 년도 불러오기
 
+        btnYearBefore.setOnClickListener{
+            year--
+            SetYearText(year)
+            Log.d(LOG_TAG,IsLeapYear(year).toString())
+        }
+        btnYearAefore.setOnClickListener{
+            year++
+            SetYearText(year)
+            Log.d(LOG_TAG,IsLeapYear(year).toString())
+        }
 
-
+        // ~구현부
 
 //        var sec: Int = 0
 //        var isRun: Boolean = false
